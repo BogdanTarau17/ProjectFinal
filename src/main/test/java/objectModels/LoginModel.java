29730cedd2e563063cd980ec060a36d94265a96f
BogdanTarau17 <bogdantarau86@gmail.com>
2024-11-22 17:21:17 +0200

    Added Utils, Data Providers, ObjectModels packages

---
+++ src/main/test/java/objectModels/LoginModel.java
@@ -0,0 +1,38 @@
+package objectModels;
+
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.Getter;
+
+@Getter
+@XmlRootElement
+public class LoginModel {
+    private AccountModel account;
+    private String loginErr;
+
+    public LoginModel() {
+    }
+
+    public LoginModel(String username, String password, String loginErr) {
+        this.account = new AccountModel(username, password);
+        this.loginErr = loginErr;
+    }
+
+    @XmlElement
+    public void setAccount(AccountModel account) {
+        this.account = account;
+    }
+
+    @XmlElement
+    public void setLoginErr(String loginErr) {
+        this.loginErr = loginErr;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginModel value: {\n" +
+                "account:{\nusername" + account.getUsername() + ",\n password=" + account.getPassword() +
+                "\n}, \n loginErr='" + loginErr + '\n' +
+                '}';
+    }
+}
